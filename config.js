2538b3deebe2962951f218bbb15df337e30bef55
Sajid Montachir <70360283+sajidmontachirr@users.noreply.github.com>
2023-11-13 22:38:42 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://simple-proxy.samonide.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMmI2MThlYjQ4YjZjZmUwMWFjMjExN2NhOWE0YWIwNSIsInN1YiI6IjY1NTFlODMwOTY1M2Y2MTNmNTg4YTE4MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C-ZKRmiUQM5YRy0dr017aD7vAn_pyKDeWGZYRqVpc6A"
+};
